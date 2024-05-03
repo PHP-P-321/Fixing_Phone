2481fc4c0a36d101049fc5d4eda6b00d4a4eb275
TvoiiSon <andreylastovetskiy@gmail.com>
2024-05-03 10:00:24 +0300

    initial commit

---
+++ db/db.php
@@ -0,0 +1,4 @@
+<?php
+// Устанавливаем соединение с базой данных MySQL
+// Аргументы функции mysqli_connect(): хост, пользователь, пароль, название базы данных
+$connect = mysqli_connect("localhost", "root", "", "fix_phone");
